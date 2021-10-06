69131f01c287b1325af2385c50d2671a2fdfab3e
yjh0410 <1394571815@qq.com>
2021-10-06 14:48:22 +0800

    update

--- utils/loss.py
+++ utils/loss.py
@@ -1,8 +1,8 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-import box_ops
-from distributed_utils import is_dist_avail_and_initialized, get_world_size
+from .box_ops import box_cxcywh_to_xyxy, generalized_box_iou
+from .distributed_utils import is_dist_avail_and_initialized, get_world_size
 
 
 def sigmoid_focal_loss(inputs, targets, num_boxes, alpha: float = 0.25, gamma: float = 2):
@@ -105,9 +105,9 @@ class SetCriterion(nn.Module):
         losses = {}
         losses['loss_bbox'] = loss_bbox.sum() / num_boxes
 
-        loss_giou = 1 - torch.diag(box_ops.generalized_box_iou(
-            box_ops.box_cxcywh_to_xyxy(src_boxes),
-            box_ops.box_cxcywh_to_xyxy(target_boxes)))
+        loss_giou = 1 - torch.diag(generalized_box_iou(
+            box_cxcywh_to_xyxy(src_boxes),
+            box_cxcywh_to_xyxy(target_boxes)))
         losses['loss_giou'] = loss_giou.sum() / num_boxes
         return losses
 
