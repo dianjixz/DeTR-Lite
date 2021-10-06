348010d5246081c755aa5362eb6c92191a88a3c9
yjh0410 <1394571815@qq.com>
2021-10-06 14:46:36 +0800

    update

--- utils/match.py
+++ utils/match.py
@@ -2,7 +2,7 @@ import torch
 from scipy.optimize import linear_sum_assignment
 from torch import nn
 
-from box_ops import box_cxcywh_to_xyxy, generalized_box_iou
+from .box_ops import box_cxcywh_to_xyxy, generalized_box_iou
 
 
 class HungarianMatcher(nn.Module):
