07b6835c3563a70778d0afb0b6b802c7afade7f7
HIT-k544 <19B908049@stu.hit.edu.cn>
2021-10-11 11:34:04 +0800

    update transforms

--- data/transforms.py
+++ data/transforms.py
@@ -111,7 +111,7 @@ def resize(image, target, size, max_size=None):
     ratios = tuple(float(s) / float(s_orig) for s, s_orig in zip(rescaled_image.size, image.size))
     ratio_width, ratio_height = ratios
 
-    target = targecopy()
+    target = target.copy()
     if "boxes" in target:
         boxes = target["boxes"]
         scaled_boxes = boxes * torch.as_tensor([ratio_width, ratio_height, ratio_width, ratio_height])
