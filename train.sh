88e2e4e22f080f3098ecc2977a2ecfd144e7450e
yjh0410 <1394571815@qq.com>
2021-11-18 17:15:22 +0800

    upate

--- train.sh
+++ train.sh
@@ -4,6 +4,8 @@ python train.py \
         -root /mnt/share/ssd2/dataset \
         --batch_size 16 \
         --img_size 640 \
+        --max_epoch 150 \
+        --lr_drop 100 \
         --aux_loss \
         --use_nms \
         # --batch_first \
